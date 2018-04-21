5503eb1fa7614f1b3dc29cf63b89968231bb4a1e
Sriwantha Attanayake <sri@DEV4>
2018-04-21 08:59:42 +0000

    v1

---
+++ MyBatchApp/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace MyBatchApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Task> jobs = new List<Task>();
+            //start 20 background tasks that will keep the CPU bussy
+            for (int i = 0; i < 20; i++)
+            {
+                string id = "worker " + i;
+                jobs.Add(Task.Factory.StartNew(new Action<object>(Run), id));
+            }
+            //do not exit, continue forever
+            Task.WaitAll(jobs.ToArray());
+        }
+
+        public static void Run(Object param)
+        {
+            while (true)
+            {
+                string stamp = DateTime.Now.ToString("HH:mm:ss") + "-->" + Math.Sqrt(DateTime.Now.Millisecond);
+                Console.WriteLine($"{param}:{stamp}");
+            }
+        }
+    }
+}
