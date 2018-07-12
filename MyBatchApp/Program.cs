4317712af33924e4d8a36f4d93d570bd28a099f6
sriwanth <sriwanth@SYD12-O9K98K3>
2018-07-13 09:13:47 +1000

    v2 make the job a bit slow

--- MyBatchApp/Program.cs
+++ MyBatchApp/Program.cs
@@ -10,7 +10,7 @@ namespace MyBatchApplication
         {
             List<Task> jobs = new List<Task>();
             //start 20 background tasks that will keep the CPU bussy
-            for (int i = 0; i < 20; i++)
+            for (int i = 0; i < 1; i++)
             {
                 string id = "worker " + i;
                 jobs.Add(Task.Factory.StartNew(new Action<object>(Run), id));
@@ -23,6 +23,7 @@ namespace MyBatchApplication
         {
             while (true)
             {
+                System.Threading.Thread.Sleep(500);
                 string stamp = DateTime.Now.ToString("HH:mm:ss") + "-->" + Math.Sqrt(DateTime.Now.Millisecond);
                 Console.WriteLine($"{param}:{stamp}");
             }
